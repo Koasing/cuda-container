b7d038d1049ba7bafec6f65e4b5b4f727fc30593
Daehyun Pak <daehyun.pak@petnow.io>
2020-10-21 15:55:38 +0900

    Backport modifications to CUDA 10.1
      - Tensorflow 2.3.x supports CUDA 10.1 only.

---
+++ dist/10.1/ubuntu18.04-x86_64/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+PYTHON_NAME=python3.8-buster
+IMAGE_NAME=petnow/cuda
+CUDA_VERSION=10.1
+CUDNN_VERSION=cudnn7
+
+docker build -t ${IMAGE_NAME}:${CUDA_VERSION}-base-${PYTHON_NAME} ./base
+
+docker build -t ${IMAGE_NAME}:${CUDA_VERSION}-runtime-${PYTHON_NAME} ./runtime \
+    --build-arg IMAGE_NAME=${IMAGE_NAME} \
+    --build-arg PYTHON_NAME=${PYTHON_NAME}
+
+docker build -t ${IMAGE_NAME}:${CUDA_VERSION}-${CUDNN_VERSION}-runtime-${PYTHON_NAME} ./runtime/${CUDNN_VERSION} \
+    --build-arg IMAGE_NAME=${IMAGE_NAME} \
+    --build-arg PYTHON_NAME=${PYTHON_NAME}
